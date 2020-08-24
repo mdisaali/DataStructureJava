03bf2a3a950608520240cad0583b80ceecb887aa
IAli00 <isa.ali@ingarmmicro.com>
2020-08-25 01:44:42 +0530

    merge two sorted arrays withpout using extra space with O(m+n)

--- TestClass.java
+++ TestClass.java
@@ -210,6 +210,31 @@ public class TestClass extends ABC {
         }
         return -1;
     }
+
+    //merge two sorted arrays O(m+n)
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        int i=m+n-1;m--;n--;
+
+        while(i>=0){
+            if(m==-1){
+                nums1[i--]=nums2[n--];
+                continue;
+            }
+
+            if(n==-1){
+                nums1[i--]=nums1[m--];
+                continue;
+            }
+
+            if( nums1[m]<=nums2[n]){
+                nums1[i--]=nums2[n--];
+            }else{
+                nums1[i--]=nums1[m--];
+            }
+        }
+
+    }
 }
 
 
